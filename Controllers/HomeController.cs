9ce8d8593f056ccadb9acfd189a7f2d5609c12d5
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2022-10-01 17:33:13 +0300

    Add Category Repo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,12 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View(Repository.Movies);
+        return View(ProductRepository.Movies);
     }
     
     public IActionResult Details(int id)
     {
-        return View(Repository.GetById(id));
+        return View(ProductRepository.GetById(id));
     }
 
     public IActionResult Contact()
