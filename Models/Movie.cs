3e8aac8079bbfe32b110350c524b647a00b63e91
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2022-10-01 17:28:26 +0300

    add description in details page

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -11,6 +11,8 @@ namespace movieApp.Models
 
         public string Name { get; set; }
 
+        public string ShortDescription { get; set; }
+
         public string Description { get; set; }
 
         public string ImageUrl { get; set; }
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -11,11 +11,11 @@ namespace movieApp.Models
 
         static Repository(){
             _movies = new List<Movie>(){
-                new Movie() {Id=1,Name="The Bridge",Description="When a body is found on the bridge between Denmark and Sweden, right on the border, Danish inspector Martin Rohde and Swedish Saga Norén have to share jurisdiction and work together to find the killer.",ImageUrl="1.jpg"},
-                new Movie() {Id=2,Name="North Sea Connection",Description="The stunning scenery of Ireland's west coast conceals a dark secret in 'North Sea Connection', in which Ciara must confront the dramatic consequences of her brother Aidan's decision to transport drugs at sea.",ImageUrl="2.jpg"},
-                new Movie() {Id=3,Name="The Square",Description="A prestigious Stockholm museum's chief art curator finds himself in times of both professional and personal crisis as he attempts to set up a controversial new exhibit.",ImageUrl="3.jpg"},
-                new Movie() {Id=4,Name="Border",Description="A customs officer who can smell fear develops an unusual attraction to a strange traveler while aiding a police investigation which will call into question her entire existence.",ImageUrl="4.jpg"},
-                new Movie() {Id=5,Name="Love & Anarchy",Description="When Sofie, a career-driven consultant and married mother of two, gets an assignment to modernize an old publishing house, she meets young IT tech Max and an unexpected flirting game begins.",ImageUrl="5.jpg"}
+                new Movie() {Id=1,Name="The Bridge",ShortDescription="The Bridge",Description="<p>When a body is found on the bridge between Denmark and Sweden, right on the border, Danish inspector Martin Rohde and Swedish Saga Norén have to share jurisdiction and work together to find the killer.</p>",ImageUrl="1.jpg"},
+                new Movie() {Id=2,Name="North Sea Connection",ShortDescription="North Sea Connection",Description="<p>The stunning scenery of Ireland's west coast conceals a dark secret in 'North Sea Connection', in which Ciara must confront the dramatic consequences of her brother Aidan's decision to transport drugs at sea.</p>",ImageUrl="2.jpg"},
+                new Movie() {Id=3,Name="The Square",ShortDescription="The Square",Description="<p>A prestigious Stockholm museum's chief art curator finds himself in times of both professional and personal crisis as he attempts to set up a controversial new exhibit.</p>",ImageUrl="3.jpg"},
+                new Movie() {Id=4,Name="Border",ShortDescription="Border",Description="<p>A customs officer who can smell fear develops an unusual attraction to a strange traveler while aiding a police investigation which will call into question her entire existence.</p>",ImageUrl="4.jpg"},
+                new Movie() {Id=5,Name="Love & Anarchy",ShortDescription="Love & Anarchy",Description="<p>When Sofie, a career-driven consultant and married mother of two, gets an assignment to modernize an old publishing house, she meets young IT tech Max and an unexpected flirting game begins.</p>",ImageUrl="5.jpg"}
             };
         }
         public static List<Movie> Movies
