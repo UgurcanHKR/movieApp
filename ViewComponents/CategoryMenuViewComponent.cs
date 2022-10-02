c37a43f76821c3519a197d1bb0d7021e7ea754da
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2022-10-02 19:30:49 +0300

    add active to category menu

--- ViewComponents/CategoryMenuViewComponent.cs
+++ ViewComponents/CategoryMenuViewComponent.cs
@@ -9,7 +9,15 @@ namespace movieApp.ViewComponents
 {
     public class CategoryMenuViewComponent: ViewComponent
     {
-        public IViewComponentResult Invoke(){
+        // {controller}/{action}/{id?}
+        // /home/index/3
+        // RouteData.Values["controller"] => home
+        // RouteData.Values["action"] => index
+        // RouteData.Values["id"] => id 3
+        public IViewComponentResult Invoke()
+        {
+            if(RouteData.Values["action"].ToString()=="Index")
+                ViewBag.SelectedCategory = RouteData?.Values["id"];
             return View(CategoryRepository.Categories);
         }  
     }
